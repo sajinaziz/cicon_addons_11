91448b004b0917517f1b4fdd94480e6697241bf9
Lali <lalitp04@gmail.com>
2016-10-20 10:59:13 +0400

    [IMP] Add Security and Fix the form loading issue depends on the module

--- cicon_job_site/__manifest__.py
+++ cicon_job_site/__manifest__.py
@@ -5,7 +5,9 @@
     'website': 'http://www.ciconuae.net',
     'category': 'Generic Modules/Others',
     'depends': ['base','mail'],
-    'data': ['views/cicon_job_site.xml'
+    'data': ['views/cicon_job_site.xml',
+             'security/cicon_job_security.xml',
+             'security/ir.model.access.csv'
             ],
     'update_xml': [],
     'description': 'Cicon Modules', 'active': False, 'installable': True, 'application': False, 'auto_install': False
