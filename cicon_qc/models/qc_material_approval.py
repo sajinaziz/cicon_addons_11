bb5f9544d91bbc3faf98f0b2b262708942b14376
sajinaziz <sajin.aziz@cicondubai.ae>
2017-02-02 10:13:43 +0400

    [FIX] qc material state change and view in job site

--- cicon_qc/models/qc_material_approval.py
+++ cicon_qc/models/qc_material_approval.py
@@ -14,8 +14,13 @@ class QcMaterialApproval(models.Model):
     _name = 'qc.material.approval'
     _description = "QC Material Approval"
     _inherit = ['mail.thread']
+    _rec_name = 'display_name'
 
+    def _display_name(self):
+        for rec in self:
+            rec.display_name = rec.job_site_id.name + ' / ' + rec.origin_attrib_value_id.name
 
+    display_name = fields.Char("Record", compute=_display_name)
     job_site_id = fields.Many2one('cicon.job.site', string="Job Site")
     date = fields.Date('Date', default=fields.Date.context_today)
     partner_id = fields.Many2one('res.partner', related='job_site_id.partner_id', string='Customer', readonly=True)
@@ -23,10 +28,33 @@ class QcMaterialApproval(models.Model):
     client_id = fields.Many2one('res.partner', related='job_site_id.consultant_id', string='Client' , readonly=True)
     origin_attrib_value_id = fields.Many2one('product.attribute.value',
                                              domain="[('attribute_id.name','=','Steel Origin' )]", string='Origin')
-    state = fields.Selection([('pending','Pending'),('verbal','Verbal'), ('approve','Approved'),('reject', 'Rejected')], string='Status', default='pending', track_visibility='onchange')
+    state = fields.Selection([('pending', 'Pending'),
+                              ('verbal', 'Verbal'), ('approve', 'Approved'),
+                              ('reject', 'Rejected')], string='Status', default='pending', track_visibility='onchange')
     remarks = fields.Text('Remarks')
 
     _sql_constraints = [('uniq_rec', 'UNIQUE(job_site_id,origin_attrib_value_id)', 'Origin must be unique for Job Site!')]
+
+
+    @api.multi
+    def set_pending(self):
+        self.ensure_one()
+        self.write({'state': 'pending'})
+
+    @api.multi
+    def set_approve(self):
+        self.ensure_one()
+        self.write({'state': 'approve'})
+
+    @api.multi
+    def set_verbal(self):
+        self.ensure_one()
+        self.write({'state': 'verbal'})
+
+    @api.multi
+    def set_reject(self):
+        self.ensure_one()
+        self.write({'state': 'reject'})
 
 
 class CiconJobSite(models.Model):
