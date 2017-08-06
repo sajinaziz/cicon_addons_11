e8c79d2b2da16ac9fa4754155ad718238790a24c
sajinaziz <sajin.aziz@cicondubai.ae>
2017-08-06 12:47:41 +0400

    [IMP] Report with selection of Origin & other fixes

--- cicon_qc/report/qc_material_approval_report.py
+++ cicon_qc/report/qc_material_approval_report.py
@@ -10,12 +10,22 @@ class MaterialApprovalReport(models.AbstractModel): # Report File Name
     def render_html(self, docids, data=None):
         report_obj = self.env['report']
         report = report_obj._get_report_from_name('cicon_qc.qc_material_approval_report_template')
-        _docs = self.env['cicon.job.site'].search([('id', 'in', docids)])
+        if data and data['context'].get('active_ids', False):
+            active_ids = data['context'].get('active_ids', False)
+        else:
+            active_ids = docids
+        _docs = self.env['cicon.job.site'].search([('id', 'in', active_ids)])
+        _origin_ids = data and data.get('origin_ids', False)
+        if _origin_ids:
+            _steel_origins = self.env['product.attribute.value'].search([('id', 'in', _origin_ids)])
+        else:
+            _steel_origins = self.env['product.attribute.value'].search([('attribute_id.name', '=', 'Steel Origin')])
+
         docargs = {
              'doc_ids': docids,
              'doc_model': report.model,
              'docs': _docs,
-             'get_origins': self._get_origins,
+             'get_origins': _steel_origins,
              'get_approval': self._get_material_approved_project,
              'get_state_list': self._get_state_list,
              'get_datetime': self._get_current_datetime
@@ -24,10 +34,6 @@ class MaterialApprovalReport(models.AbstractModel): # Report File Name
 
     def _get_state_list(self):
         return dict(self.env['qc.material.approval'].fields_get(allfields=['state'])['state']['selection'])
-
-    def _get_origins(self):
-        _steel_origins = self.env['product.attribute.value'].search([('attribute_id.name','=','Steel Origin')])
-        return _steel_origins
 
     def _get_material_approved_project(self, _site_id):
         _approved_list = self.env['qc.material.approval'].search([('job_site_id','=', _site_id)])
