5bc04b1a56598b0dd6c1cb81d8c56fbaa352a8de
sajinaziz <sajin.aziz@cicondubai.ae>
2017-08-07 09:29:04 +0400

    [FIX] Sorting Issue

--- cicon_qc/report/qc_material_approval_report.py
+++ cicon_qc/report/qc_material_approval_report.py
@@ -14,7 +14,8 @@ class MaterialApprovalReport(models.AbstractModel): # Report File Name
             active_ids = data['context'].get('active_ids', False)
         else:
             active_ids = docids
-        _docs = self.env['cicon.job.site'].search([('id', 'in', active_ids)])
+        _sites = self.env['cicon.job.site'].search([('id', 'in', active_ids)])
+        _docs = _sites.sorted(key=lambda a: (a.partner_id.name, a.name))
         _origin_ids = data and data.get('origin_ids', False)
         if _origin_ids:
             _steel_origins = self.env['product.attribute.value'].search([('id', 'in', _origin_ids)])
