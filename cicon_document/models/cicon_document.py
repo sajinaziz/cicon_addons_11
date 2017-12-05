27ffba0f36d9c310620a346cb514fd5899b8511f
sajinaziz <sajin.aziz@cicondubai.ae>
2017-12-05 16:02:58 +0400

    [IMP] Document With Revision and Properties on Directory

--- cicon_document/models/cicon_document.py
+++ cicon_document/models/cicon_document.py
@@ -1,37 +1,117 @@
 from odoo import models,fields,api
 
 
+class CiconDocumentDirectoryProperty(models.Model):
+    _name = 'cicon.document.directory.property'
+    _description = "Directory Property"
+
+    name = fields.Char('Property Name', required=True)
+
+    _sql_constraints = [('uniq_name', 'UNIQUE(name)', "Property Name Should be Unique" )]
+
+
 class CiconDocumentDirectory(models.Model):
     _name = 'cicon.document.directory'
     _description = "CICON Document Directory"
 
     name = fields.Char('Directory Name', required=True)
-    parent_id = fields.Many2one('cicon.document.directory', string='Parent Directory')
+    # parent_id = fields.Many2one('cicon.document.directory', string='Parent Directory')
+    property_ids = fields.Many2many('cicon.document.directory.property', 'cicon_doc_dir_property_rel',
+                                    'directory_id', "property_id", string="Properties")
+
+    _sql_constraints = [('uniq_dir_name', 'UNIQUE(name)', "Directory Name Should be Unique")]
 
 
-class Documents(models.Model):
+class CiconDocumentPropertyValue(models.Model):
+    _name = 'cicon.document.property.value'
+    _description = "Document Directory Property"
+
+    document_id = fields.Many2one('cicon.document', string="Document", ondelete='cascade')
+    property_id = fields.Many2one('cicon.document.directory.property', "Property", required=True)
+    property_value = fields.Char('Value', required=True)
+
+    _sql_constraints = [('uniq_name', 'UNIQUE(document_id,property_id)', "Property Should be Unique")]
+
+
+class CiconDocument(models.Model):
     _name = 'cicon.document'
     _description = "CICON Documents"
 
-    # @api.depends('res_model', 'res_id')
-    # def _compute_reference(self):
-    #     for res in self:
-    #         if res.res_model and res.res_id:
-    #             res.reference = "%s,%s" % (res.res_model, res.res_id)
+    @api.depends('res_model', 'res_id')
+    def _compute_reference(self):
+        for _rec in self:
+            if _rec.rev_number > 0:
+                _rec.display_name = _rec.name + "-Rev." + str(_rec.rev_number)
+            else:
+                _rec.display_name = _rec.name
 
-    name = fields.Char('Document Name', required=True)
+            if _rec.res_model and _rec.res_id:
+                _rec.reference = "%s,%s" % (_rec.res_model, _rec.res_id)
+
+    display_name = fields.Char(compute=_compute_reference , string="Document")
+    name = fields.Char('Document Name', required=True,copy=False)
+    parent_id = fields.Many2one('cicon.document', string='Parent Document',copy=False)
+    rev_number = fields.Integer('Revision', default=0, required=True,copy=False)
+
     dir_id = fields.Many2one('cicon.document.directory', string='Directory', required=True)
-    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id)
+    doc_code = fields.Char('Document ID')
     note = fields.Text('Notes')
     description = fields.Char("Description")
     is_private = fields.Boolean('Private', default=False)
-    expiry_date = fields.Date('Document Expiry')
+    expiry_date = fields.Date('Document Expiry', copy=False)
     res_model = fields.Char('Resource Model')
     res_id = fields.Integer('Resource ID')
-    #reference = fields.Char(string='Reference', compute='_compute_reference', readonly=True, store=False)
+    reference = fields.Char(string='Reference', compute='_compute_reference', readonly=True, store=False)
     attachment_ids = fields.Many2many('ir.attachment', 'cicon_document_attachment_rel', 'cicon_document_id',
                                       'attachment_id', 'Attachments',
-                                      help="You may attach files to this Document")
+                                      help="You may attach files to this Document", copy=False)
+    property_ids = fields.Many2many(related='dir_id.property_ids', store=False, string="Properties")
+    property_value_ids = fields.One2many('cicon.document.property.value', 'document_id', string="Property Values")
+
+    doc_date = fields.Date('Date', default=fields.Date.today(), copy=False)
+    user_id = fields.Many2one('res.users', string="User", default=lambda self: self.env.user, copy=False)
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id, copy=False)
+    state = fields.Selection([('active', 'Active'), ('inactive', 'In-Active')], string="Status", default='active', copy=False)
+
+
+    #_sql_constraints = [('uniq_doc_code', 'UNIQUE(dir_id,doc_code,rev_number)', "Document Code Should be Unique")]
+
+    @api.onchange('parent_id')
+    def onchange_parent(self):
+        if self.parent_id:
+            self.name = self.parent_id.name
+            self.rev_number = self.parent_id.rev_number +1
+            self.dir_id = self.parent_id.dir_id
+            self.doc_code = self.parent_id.doc_code
+            self.note = self.parent_id.note
+            self.description = self.parent_id.description
+            self.is_private = self.parent_id.is_private
+            self.res_model = self.parent_id.res_model
+            self.res_id = self.parent_id.res_id
+
+    def revise_document(self):
+        self.ensure_one()  # One Record
+        form_id = self.env.ref('cicon_document.cicon_document_base_form')
+        ctx = dict(
+            default_parent_id=self.id,
+        )
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'cicon.document',
+            'views': [(form_id.id, 'form')],
+            'view_id': form_id.id,
+            'target': 'current',
+            'context': ctx,
+        }
+
+    @api.model
+    def create(self, vals):
+        _res = super(CiconDocument, self).create(vals=vals)
+        if _res.parent_id:
+            _res.parent_id.write({'state': 'inactive'})
+        return _res
 
 
 
