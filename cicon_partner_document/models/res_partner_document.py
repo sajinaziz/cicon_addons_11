d32a6d133f7713e0e5911d9e4221487304901f15
sajinaziz <sajin.aziz@cicondubai.ae>
2017-12-05 16:09:38 +0400

    [IMP] Link Partner and Document as many2many

--- cicon_partner_document/models/res_partner_document.py
+++ cicon_partner_document/models/res_partner_document.py
@@ -1,13 +1,16 @@
 from odoo import models,fields,api
 
 
-class CiconDocument(models.Model):
-    _inherit = 'cicon.document'
-
-    partner_id = fields.Many2one('res.partner', string="Customer")
+# class CiconDocument(models.Model):
+#     _inherit = 'cicon.document'
+#
+#     partner_id = fields.Many2one('res.partner', string="Customer")
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    document_ids = fields.One2many('cicon.document', 'partner_id', groups="cicon_document.cicon_document_user", string="Documents")
+    document_ids = fields.Many2many('cicon.document', relation='cicon_partner_document_rel' , column1='partner_id',
+                                    column2='document_id', groups="cicon_document.cicon_document_user", string="Documents",
+                                    domain=[('state', '=', 'active')]
+                                    )
