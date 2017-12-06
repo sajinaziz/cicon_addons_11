7f1704ca36aaf37d9f53c6b49f3718a9da8287b6
sajinaziz <sajin.aziz@cicondubai.ae>
2017-12-06 10:04:36 +0400

    [IMP] New Document Can be create direct from partner form & Other minor imps

--- cicon_partner_document/models/res_partner_document.py
+++ cicon_partner_document/models/res_partner_document.py
@@ -1,10 +1,10 @@
 from odoo import models,fields,api
 
 
-# class CiconDocument(models.Model):
-#     _inherit = 'cicon.document'
-#
-#     partner_id = fields.Many2one('res.partner', string="Customer")
+class CiconDocument(models.Model):
+    _inherit = 'cicon.document'
+
+    partner_ids = fields.Many2many('res.partner', relation='cicon_partner_document_rel', column1='document_id', column2='partner_id', string="Customers")
 
 
 class ResPartner(models.Model):
@@ -14,3 +14,22 @@ class ResPartner(models.Model):
                                     column2='document_id', groups="cicon_document.cicon_document_user", string="Documents",
                                     domain=[('state', '=', 'active')]
                                     )
+
+    def customer_new_doc(self):
+        self.ensure_one()  # One Record
+        form_id = self.env.ref('cicon_document.cicon_document_base_form')
+        ctx = dict(
+            default_res_model='res.partner',
+            default_res_id=self.id,
+            default_partner_ids =[self.id]
+        )
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'cicon.document',
+            'views': [(form_id.id, 'form')],
+            'view_id': form_id.id,
+            'target': 'current',
+            'context': ctx,
+        }
