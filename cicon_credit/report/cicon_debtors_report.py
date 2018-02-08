d70d31cf0be5cb0a41e6f6bfcc4e30f903081284
sajinaziz <sajin.aziz@cicondubai.ae>
2018-02-08 15:40:39 +0400

    [FIX] Report Layout with All Data

--- cicon_credit/report/cicon_debtors_report.py
+++ cicon_credit/report/cicon_debtors_report.py
@@ -1,13 +1,11 @@
 from odoo import api, models
 
 _res_openerp_data = []
-_res_sun_data =[]
-
 
 class CiconDebtorsReport(models.AbstractModel):
     _name = 'report.cicon_credit.report_cicon_debtors_report_template'
 
-    def _get_sun_data(self,sun_account):
+    def _get_sun_data(self, sun_account):
         _res = {}
         _qry = "EXEC dbo.Get_Aging @SunAccountNo = '" + sun_account + "'"
         _res_qry = self.env['odbc.db.source'].fetch_data('SUN_DB', _qry)
@@ -20,6 +18,7 @@ class CiconDebtorsReport(models.AbstractModel):
         _res = {}
         _qry = "EXEC dbo.Get_Aging @SunAccountNo = ''"
         _res_qry = self.env['odbc.db.source'].fetch_data('SUN_DB', _qry)
+        #_res_qry = [{'a': 1, 'b': 2}]
         if _res_qry:
             _res = _res_qry
         return _res
@@ -38,11 +37,11 @@ class CiconDebtorsReport(models.AbstractModel):
         #         _res.append(self._get_sun_data(sun_account.sun_account_no))
         # else:
         #     _res = self._get_all_sun_data()
-        self._res_sun_data = self._get_all_sun_data()
-        for _r in _res_sun_data:
-            for _key in [*_r]:
-                if _r[_key] == 0 or _r[_key] == '0.0' or _r[_key] == '0':
-                    _r[_key] = 0
+        #self._res_sun_data = self._get_all_sun_data()
+        # for _r in _res_sun_data:
+        #     for _key in [*_r]:
+        #         if _r[_key] == 0 or _r[_key] == '0.0' or _r[_key] == '0':
+        #             _r[_key] = 0
         self._res_openerp_data = self._get_all_partners_from_openerp()
 
     def _get_partners(self):
@@ -55,6 +54,7 @@ class CiconDebtorsReport(models.AbstractModel):
         _check_data = list(filter(lambda d: d['id'] == partner_id,  self._res_openerp_data))
         return _check_data
 
+
     @api.multi
     def get_report_values(self, docids, data=None):
         _company = self.env['res.company'].search([('id', 'in', docids)])
@@ -66,6 +66,6 @@ class CiconDebtorsReport(models.AbstractModel):
             'docs': _company,
             'get_partners': self._get_partners,
             'get_check_details': self._get_report_check_data_for_partner,
-            'get_sun_details': self._get_all_sun_data
+            'get_sun_details': self._get_sun_data
 
         }
