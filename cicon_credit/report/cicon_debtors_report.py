983eca6465e34339c454381d33d8fbca865028e0
sajinaziz <sajin.aziz@cicondubai.ae>
2018-02-08 14:52:23 +0400

    [FIX] Report Layout with All Data

--- cicon_credit/report/cicon_debtors_report.py
+++ cicon_credit/report/cicon_debtors_report.py
@@ -1,8 +1,10 @@
 from odoo import api, models
 
-
+_res_openerp_data = []
+_res_sun_data =[]
 class CiconDebtorsReport(models.AbstractModel):
     _name = 'report.cicon_credit.report_cicon_debtors_report_template'
+
 
     def _get_sun_data(self,sun_account):
         _res = {}
@@ -23,33 +25,54 @@ class CiconDebtorsReport(models.AbstractModel):
         return _res
 
     def _get_all_partners_from_openerp(self):
-        _qry = "select * from GetOpenErpData('' )"
+        _qry = "SELECT * FROM GetOpenErpData('')"
         _cr = self._cr
         _cr.execute(_qry)
         _res = _cr.dictfetchall()
         return _res
 
-    def _get_report_data(self, _partner):
-        _res =[]
-        if _partner.sun_account_ids:
-            for sun_account in _partner.sun_account_ids:
-                _res.append(self._get_sun_data(sun_account.sun_account_no))
-        else:
-            _res = self._get_all_sun_data()
-        for _r in _res:
+    def _get_report_data(self, _partner=None):
+        # _res =[]
+        # if _partner.sun_account_ids:
+        #     for sun_account in _partner.sun_account_ids:
+        #         _res.append(self._get_sun_data(sun_account.sun_account_no))
+        # else:
+        #     _res = self._get_all_sun_data()
+        self._res_sun_data = self._get_all_sun_data()
+        for _r in _res_sun_data:
             for _key in [*_r]:
                 if _r[_key] == 0 or _r[_key] == '0.0' or _r[_key] == '0':
                     _r[_key] = 0
+        self._res_openerp_data = self._get_all_partners_from_openerp()
+
+    def _get_partners(self):
+        _res_list = [(_partner.get('id'), _partner.get('name')) for _partner in self._res_openerp_data]
+        _res_set = list(set(_res_list))
+        _res = sorted(_res_set, key=lambda x: x[1])
+        return _res
+
+    def _get_report_check_data_for_partner(self, partner_id):
+        _check_data = list(filter(lambda d: d['id'] == partner_id,  self._res_openerp_data))
+        return _check_data
+
+    def _get_report_sun_data_for_sun_code(self, sun_code):
+        _res = {}
+        _sun_data = list(filter(lambda d: d['ACCNT_CODE'] == sun_code,  self._res_sun_data))
+        if _sun_data:
+            _res = _sun_data[0]
         return _res
 
     @api.multi
     def get_report_values(self, docids, data=None):
-        _partners = self.env['res.partner'].search([('id', 'in', docids)])
+        _company = self.env['res.company'].search([('id', 'in', docids)])
+        self._get_report_data()
 
         return {
-            'doc_ids': _partners.ids,
+            'doc_ids': _company.ids,
             'doc_model': 'res.partner',
-            'docs': _partners,
-            'get_report_data': self._get_report_data,
-            'get_partners' : self._get_all_partners_from_openerp
+            'docs': _company,
+            'get_partners': self._get_partners,
+            'get_check_details': self._get_report_check_data_for_partner,
+            'get_sun_details': self._get_report_sun_data_for_sun_code
+
         }
--- cicon_credit/wizard/cicon_debtors_report_wizard.py
+++ cicon_credit/wizard/cicon_debtors_report_wizard.py
@@ -7,10 +7,11 @@ class CiconDebtorsReportWizard(models.TransientModel):
 
     start_date = fields.Date('From')
     end_date = fields.Date('To')
-    partner_ids = fields.Many2many('res.partner',relation='cicon_debtor_report_partner_rel',column1='wizard_id',
-                                   column2='partner_id', string='Partners')
+    # partner_ids = fields.Many2many('res.partner',relation='cicon_debtor_report_partner_rel',column1='wizard_id',
+    #                                column2='partner_id', string='Partners')
 
     @api.multi
     def show_report(self):
-        return self.env.ref('cicon_credit.action_cicon_debtors_report').report_action(self.partner_ids)
+        _company = self.env.user.company_id
+        return self.env.ref('cicon_credit.action_cicon_debtors_report').report_action(_company)
 
