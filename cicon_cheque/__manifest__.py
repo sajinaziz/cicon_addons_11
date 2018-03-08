225a601a8da5301063a8040022273b06c31d9b5c
sajinaziz <sajin.aziz@cicondubai.ae>
2018-03-08 11:38:03 +0400

    [ADD] New Module For Cheque

---
+++ cicon_cheque/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'CICON Cheque',
+    'version': '1.1',
+    'author': 'CICON',
+    'website': 'http://www.openerp.com',
+    'category': 'Finance',
+    'depends': ['base', 'account'],
+    'description': """CICON Cheque Management.""",
+    'data': [
+     'views/cicon_cheque_view.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ cicon_cheque/models/cicon_cheque.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    inbound_check_payment = fields.Boolean('Cheque Payment', default=True)
+    cheque_date = fields.Date('Cheque Date')
+    check_number_inbound = fields.Char('Cheque Number', size=32,
+                                       help="Cheque Number")
+    deposit_date = fields.Date('Submission Date')
+
+    rv_number = fields.Char('RV Voucher No', size=10,
+                            help="RV Number")
+    voucher_number = fields.Char('Collection Voucher No.')
+    received_date = fields.Date('Received Date')
+    note = fields.Text('Notes')
+    state = fields.Selection(selection_add=[('bounce', 'Bounce')])
