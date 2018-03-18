8b39adc42f24f2d440cc3e7f9c2cda503fbcae3c
sajinaziz <sajin.aziz@cicondubai.ae>
2018-03-18 11:22:07 +0400

    [ADD] Job Site and User Sign

---
+++ cicon_user_sign/__manifest__.py
@@ -0,0 +1,18 @@
+
+{
+    'name': 'CICON User Signature',
+    'version': '1.1',
+    'author': 'CICON',
+    'category': 'Others',
+    'description': """
+Module for the CICON User Signature.
+================================================
+    """,
+    'website': 'http://www.cicon.ae',
+    'depends': ['base'],
+    "data": ['views/cic_user_sign_view.xml',
+             'security/ir.model.access.csv'],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
