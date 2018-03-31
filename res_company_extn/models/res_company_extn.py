63feada51f866c0fd395c13069c8065ef1cb3b17
sajinaziz <sajin.aziz@cicondubai.ae>
2018-03-31 16:56:37 +0400

    [ADD] #Fax field Removed in Odoo 11 re added as still maintained in Region

--- res_company_extn/models/res_company_extn.py
+++ res_company_extn/models/res_company_extn.py
@@ -5,6 +5,9 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     logo2 = fields.Binary('Logo 2')
+    #Fax field Removed in Odoo 11 re added as still maintained in Region
+    fax = fields.Char("Fax")
+
 
 
 
