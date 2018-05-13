1a5e6df1386fc13fd2a11503fe97d5548cb518cf
sajinaziz <sajin.aziz@cicondubai.ae>
2018-05-13 15:48:35 +0400

    [FIX] Sales Condition Fixes

--- cicon_sale_quotation/models/cicon_sale_quotation.py
+++ cicon_sale_quotation/models/cicon_sale_quotation.py
@@ -23,7 +23,24 @@ class CiconSaleCondition(models.Model):
     _sql_constraints = [('uniq_cond', 'UNIQUE(template_id,name)', 'Template name should be unique !')]
 
 
+class CiconSaleOrderCondition(models.Model):
+    _name = 'cicon.sale.order.condition'
+    _description = "Sale Order Condition"
+
+    sale_order_id = fields.Many2one('sale.order', string="Sale Order")
+    description = fields.Html('Description')
+
+
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    cicon_template_id = fields.Many2one('cicon.sale.condition.template', string="Quotation Template")
+    cicon_template_ids = fields.Many2many('cicon.sale.condition.template', 'cicon_sale_cond_temp_rel', 'sale_order_id', 'templ_id', string="Quotation Template")
+    sale_condition_ids = fields.One2many('cicon.sale.order.condition','sale_order_id', string="Sale Condition")
+
+    @api.onchange('cicon_template_ids')
+    def _on_change_temp(self):
+        _res = []
+        for _tmpl in self.cicon_template_ids:
+            for _cond in _tmpl.condition_ids:
+                _res.append({'description': _cond.description})
+        self.sale_condition_ids = _res
